5f2b138c9c34e28853017e9e146f344fbc160000
Peter Ukena <peterukena@googlemail.com>
2014-02-22 15:18:22 +0100

    fixed another classname

--- app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
+++ app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
@@ -6,7 +6,7 @@
  * Time: 11:47
  */
 
-class Hackathon_HealthCheck_Model_Sitemap extends Hackathon_HealthCheck_Model_Check_Abstract
+class Hackathon_HealthCheck_Model_Check_Sitemap extends Hackathon_HealthCheck_Model_Check_Abstract
 {
     public function getType() {
         return 'static';
