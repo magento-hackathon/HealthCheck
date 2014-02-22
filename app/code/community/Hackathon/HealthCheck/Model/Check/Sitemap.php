f26ab33ed7b8038879d0c7f1e4a910facce506f9
root <root@philipp-H61M-D2H-USB3.(none)>
2014-02-22 18:18:12 +0100

    function run in Sitemap.php

--- app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
+++ app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
@@ -15,12 +15,43 @@ class Hackathon_HealthCheck_Model_Check_Sitemap extends Hackathon_HealthCheck_Mo
 
     public function run() {
         if ($this->initCheck()) {
-            /**
-             * @TODO Do the actual check here
-             */
-            return array('result' => 'true');
+
+
+   	    $sitemaps = Mage::getModel('sitemap/sitemap')->getCollection();
+
+	    $array = array();
+
+	    foreach ($sitemaps as $sitemap) {
+    	    	$filename = $sitemap->getSitemapFilename();
+    		$time = $sitemap->getSitemapTime();
+    		$id = $sitemap->getSitemapId();
+    		$path = $sitemap->getSitemapPath();
+
+		$total_path = Mage::getBaseDir() . $path . $filename;
+
+    		if (file_exists($total_path)){
+        	    $last_mod = filemtime($total_path);
+        	    $last_change = date("d/m/Y H:i:s", $last_mod);
+
+        	    $time24 = mktime(0,0,86400,0,0,0);
+
+        	    if (time() >= $last_mod + $time24) {
+            		$chng_time = "Did not changed past 24h";
+        	    }
+        	    else {
+            		$chng_time =  "Okay";
+        	    }
+
+        	$array[] = array ($id,$filename,$total_path,$chng_time);
+    		}
+    		else {
+        	    echo "File" . $total_path . "does not exist";
+    		}
+	    }
+
+            return $array;
         } else {
-            return array('result' => 'false');
+            return array("result" => "false");
         }
     }
 }
