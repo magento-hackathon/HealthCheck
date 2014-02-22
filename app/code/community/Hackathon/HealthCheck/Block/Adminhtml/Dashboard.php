aa44685fb24e8129191b2cff059a803e5b98f1c1
Achim Rolle <achimrolle@googlemail.com>
2014-02-22 23:14:00 +0100

    Tries

--- app/code/community/Hackathon/HealthCheck/Block/Adminhtml/Dashboard.php
+++ app/code/community/Hackathon/HealthCheck/Block/Adminhtml/Dashboard.php
@@ -2,10 +2,18 @@
 
 class Hackathon_HealthCheck_Block_Adminhtml_Dashboard extends Mage_Adminhtml_Block_Template
 {
-    public function getAllChecks() {
+    public function getAllChecks()
+    {
         /** @var Hackathon_HealthCheck_Model_Check_Collection $collection */
         $collection = Mage::getModel('hackathon_healthcheck/check_collection');
 
         return $collection;
+    }
+
+    public function getCheckBlock(Hackathon_HealthCheck_Model_Check_Abstract $check)
+    {
+        $block = $this->getLayout()->createBlock('healthcheck/adminhtml_type_' . $check->getType());
+        $block->setCheck($check);
+        return $block;
     }
 }
