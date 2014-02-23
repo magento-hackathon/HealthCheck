8b8255ca6a887ffc0b69ac81e1adf44a2409acc7
Peter Ukena <peterukena@googlemail.com>
2014-02-23 10:38:13 +0100

    added new check

--- app/code/community/Hackathon/HealthCheck/Model/Check/Hiddenactivations.php
+++ app/code/community/Hackathon/HealthCheck/Model/Check/Hiddenactivations.php
@@ -4,7 +4,17 @@ class Hackathon_HealthCheck_Model_Check_Hiddenactivations extends Hackathon_Heal
 {
     public function _run() {
 
+        $helper = Mage::helper('hackathon_healthcheck');
+
         $renderer = $this->getContentRenderer();
+
+        $renderer->setHeaderRow(array(
+            $helper->__('XML File'),
+            $helper->__('"active" Flags'),
+            $helper->__('Status'))
+        );
+
+
 
         $renderer->addRow(array('testets', 'asdasd', '12312312'));
 
