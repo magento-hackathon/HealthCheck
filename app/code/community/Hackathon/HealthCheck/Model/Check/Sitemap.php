8bf12af70e01bd690dbf6c8c26d6c58d08fbf5f5
Achim Rolle <achimrolle@googlemail.com>
2014-02-23 18:02:22 +0100

    remove debug output

--- app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
+++ app/code/community/Hackathon/HealthCheck/Model/Check/Sitemap.php
@@ -33,12 +33,12 @@ class Hackathon_HealthCheck_Model_Check_Sitemap extends Hackathon_HealthCheck_Mo
                 $totalPath = Mage::getBaseDir() . $path . $filename;
 
                 if (file_exists($totalPath)){
-                    
+
                     $date = Mage::getModel('core/date')->timestamp(time());
                     $time24 = date('Y-m-d H:i:s', $date);
                     $time24 = strtotime($time24) - 86400;
 
-                    if ( $sitemapTime - $time24 > 0) {
+                    if ( $sitemapTime - $time24 < 0) {
                         $status = $helper->__('OK, but not change within last 24h');
                         $warn = array('_cssClasses' => Hackathon_HealthCheck_Model_Check_Abstract::WARN_TYPE_WARNING);
                     } else {
@@ -49,7 +49,7 @@ class Hackathon_HealthCheck_Model_Check_Sitemap extends Hackathon_HealthCheck_Mo
                     $status = $helper->__('Sitemap file not found');
                     $warn = array('_cssClasses' => Hackathon_HealthCheck_Model_Check_Abstract::WARN_TYPE_ERROR);
                 }
-                $row = array ($id, $filename . ' | ' . $sitemapTime . ' | ' .$time24, $totalPath, $status);
+                $row = array ($id, $filename, $totalPath, $status);
 
                 $this->getContentRenderer()->addRow($row, $warn);
             }
