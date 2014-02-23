d06bf4f377c5ec20b6929bf61f46068ea4d55f79
Peter Ukena <peterukena@googlemail.com>
2014-02-23 11:34:23 +0100

    Added Status/Info Check

--- app/code/community/Hackathon/HealthCheck/Model/Check/Htaccess.php
+++ app/code/community/Hackathon/HealthCheck/Model/Check/Htaccess.php
@@ -8,7 +8,52 @@ class Hackathon_HealthCheck_Model_Check_Htaccess extends Hackathon_HealthCheck_M
 
         $renderer = $this->getContentRenderer();
 
-        $renderer->setPlaintextContent(php_sapi_name());
+        $header = array(
+            $helper->__('Service'),
+            $helper->__('Status'),
+        );
+
+        $row = array();
+
+        /**
+         * Webserver interface, PHP.ini information
+         */
+        $row[$helper->__("Webserver")] = $_SERVER["SERVER_SOFTWARE"];
+        $row[$helper->__("Maximum execution time (PHP)")] = ini_get('max_execution_time');
+        $row[$helper->__("Memory Limit")] = ini_get('memory_limit');
+
+        /**
+         * HTACCESS-Check
+         */
+        if (file_exists(Mage::getBaseDir() . "/.htaccess")) {
+            $row[$helper->__('.htaccess')] = 'htaccess exists';
+        } else {
+            $row[$helper->__('.htaccess')] = 'htaccess does not exist';
+        }
+
+        /**
+         * Magento-URL Information
+         */
+        $row[$helper->__('Admin-URL')] = Mage::getUrl();
+
+        foreach (Mage::app()->getStores() as $store) {
+            $row[$store->getName()] = Mage::app()->getStore($store->getId())->getUrl();
+        }
+
+
+
+
+        /**
+         * Rendering
+         */
+        $renderer->setHeaderRow($header);
+
+        foreach ($row as $key => $line ) {
+            $renderer->addRow(array($key, $line));
+        }
+
+
+
 
         return $this;
     }
